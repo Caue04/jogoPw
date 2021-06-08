2dbe540937571cfc7d0a15964f7d4330a92c89c0
Caue <caue.zani@hotmail.com>
2021-06-08 17:37:03 -0300

    pagina inicial teste

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const pergunta = document.querySelector('.question')
-const
+let iniciar = document.querySelector(".iniciar")
+let containerQuiz = document.querySelector(".containerQuiz")
+let containerIniciar = document.querySelector(".containerIniciar")
+
+iniciar.addEventListener("click", pularPagina);
+
+function pularPagina() {
+    containerIniciar.style.display = "none";
+    containerQuiz.style.display = "block";
+}
