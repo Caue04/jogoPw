bd6b0234721865be5bcf1122ef52a55bae6d7632
Caue <caue.zani@hotmail.com>
2021-06-09 17:47:03 -0300

    1.0

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-let iniciar = document.querySelector(".iniciar")
-let containerQuiz = document.querySelector(".containerQuiz")
-let containerIniciar = document.querySelector(".containerIniciar")
+let perguntaAle, perguntaEscolha
+const botaoIniciar = document.getElementById('btnI')
+const botaoProximo = document.getElementById('proximoBtn')
+const containerQuiz = document.getElementById('quizPrincipal')
+const perguntaPrincipal = document.getElementById('perguntaJs')
+const respostasBtn = document.getElementById('rsp')
+const containerIniciarQuiz = document.getElementById('iniciarPrincipal')
+const containerPrincipalQuiz = document.getElementById('principalQ')
 
-iniciar.addEventListener("click", pularPagina);
+botaoIniciar.addEventListener('click', iniciarQuiz)
+botaoProximo.addEventListener('click', () => {
+  
+  perguntaEscolha++
+  proximaPg()
+})
 
-function pularPagina() {
-    containerIniciar.style.display = "none";
-    containerQuiz.style.display = "block";
-}
+function iniciarQuiz() {
+    containerIniciarQuiz.style.display = "none";
+    containerPrincipalQuiz.style.display = "block";
+    perguntaAle = perguntas.sort(() => Math.random() - .5)
+    perguntaEscolha = 0
+    proximaPg()
+  }
+
+  function proximaPg() {
+    mostrarPerguntas(perguntaAle[perguntaEscolha])
+  }
+
+  function mostrarPerguntas(pergunta) {
+    perguntaPrincipal.innerText = pergunta.pergunta
+  }
+
+  const perguntas = [
+    {
+      pergunta: '1 + 1',
+      respostas: [
+        {text: '2', correct: true},
+        {text: '4', correct: false},
+        {text: '6', correct: false},
+        {text: '8', correct: false}
+      ]
+    }
+  ]
