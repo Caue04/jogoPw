3c8c992f6c952c4f468f23f788297df7ffdd43e6
Caue <caue.zani@hotmail.com>
2021-06-11 19:09:01 -0300

    js começo

---
+++ script.js
@@ -0,0 +1,18 @@
+//aqui sÃ£o as var principais do jogo, contendo o turno, x na matriz
+//o na matriz e as possibilidades de vitorias no jogo 
+var pontosX = 0;
+var pontosO = 0;
+var pontoEmpate = 0;
+var turno = 0;
+var x = [];
+var o = [];
+var vitoriasJg = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+]
